780f52b85b89d5089de6918ea09f191aef3374cb
shino <spacedisks@gmail.com>
2023-01-27 12:59:59 +0530

    first commit

---
+++ blog/views.py
@@ -0,0 +1,8 @@
+
+from django.utils import timezone
+from .models import Post
+from django.shortcuts import render
+
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
