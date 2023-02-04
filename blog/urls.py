e0f64c8c4afa94ab24e47ed7e6c86f54dcadbee9
shino <spacedisks@gmail.com>
2023-02-04 10:44:30 +0530

    UI_design_V2

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('',views.post_list,name= 'post_list'),
+    path('',views.index,name= 'index'),
+    path('about',views.about,name= 'about'),
+    
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,10 @@
 
 from django.utils import timezone
-from .models import Post
+from .models import index
+from .models import about
 from django.shortcuts import render
 
-def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+def index(request):
+    return render(request, 'blog/index.html')
+def about(request):
+    return render(request, 'blog/machines.html')
